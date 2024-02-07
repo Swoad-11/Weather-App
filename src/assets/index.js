03f201a57cd324d325ab869ebc4b4600f7bb5d41
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2024-02-07 21:06:22 +0600

    added more icons from API

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+// weatherIcons.js
+
+export const cloudIcon1d = "https://openweathermap.org/img/wn/02d@2x.png";
+export const cloudIcon1n = "https://openweathermap.org/img/wn/02n@2x.png";
+export const cloudIcon2 = "https://openweathermap.org/img/wn/03d@2x.png";
+export const cloudIcon3 = "https://openweathermap.org/img/wn/04d@2x.png";
+export const dayIcon = "https://openweathermap.org/img/wn/01d@2x.png";
+export const nightIcon = "https://openweathermap.org/img/wn/01n@2x.png";
+export const hazeIcon = "https://openweathermap.org/img/wn/50d@2x.png";
+export const rainIconD = "https://openweathermap.org/img/wn/10d@2x.png";
+export const rainIconN = "https://openweathermap.org/img/wn/10n@2x.png";
+export const drizzleIcon = "https://openweathermap.org/img/wn/11d@2x.png";
+export const thunderIcon = "https://openweathermap.org/img/wn/11d@2x.png";
+export const snowIcon = "https://openweathermap.org/img/wn/13d@2x.png";
--- src/components/Weather/WeatherHeadline.jsx
+++ src/components/Weather/WeatherHeadline.jsx
@@ -3,37 +3,97 @@ import pin from "../../assets/pin.svg";
 import { WeatherContext } from "../../context";
 import { getFormattedDate } from "../../utils/dateUtil";
 
-import cloudIcon from "../../assets/cloud.svg";
-import hazeIcon from "../../assets/haze.svg";
-import sunIcon from "../../assets/sun.svg";
-import rainIcon from "../../assets/rainy.svg";
-import thunderIcon from "../../assets/thunder.svg";
-import snowIcon from "../../assets/snow.svg";
+import {
+  cloudIcon1d,
+  cloudIcon1n,
+  cloudIcon2,
+  cloudIcon3,
+  hazeIcon,
+  dayIcon,
+  nightIcon,
+  rainIconD,
+  rainIconN,
+  snowIcon,
+  thunderIcon,
+  drizzleIcon,
+} from "../../assets/index";
 
 export default function WeatherHeadline() {
   const { weatherData } = useContext(WeatherContext);
-  const { climate, temperature, location, time } = weatherData;
+  const { id, temperature, location, time } = weatherData;
 
-  function weatherIcon(climate) {
-    switch (climate) {
-      case "Clouds":
-        return cloudIcon;
-      case "Clear":
-        return sunIcon;
-      case "Haze":
+  function weatherIcon(id, time) {
+    const isDay =
+      getFormattedDate(time, "time", false).includes("AM") ||
+      (getFormattedDate(time, "time", false) >= "06:00" &&
+        getFormattedDate(time, "time", false) <= "18:00");
+    switch (id) {
+      case 801:
+        return isDay ? cloudIcon1d : cloudIcon1n;
+      case 802:
+        return cloudIcon2;
+      case 803:
+      case 804:
+        return cloudIcon3;
+      case 800:
+        return isDay ? dayIcon : nightIcon;
+      case 700:
+      case 711:
+      case 721:
+      case 731:
+      case 741:
+      case 751:
+      case 761:
+      case 762:
+      case 771:
+      case 781:
         return hazeIcon;
-      case "Fog":
-        return hazeIcon;
-      case "Mist":
-        return hazeIcon;
-      case "Rain":
-        return rainIcon;
-      case "Thunderstorm":
+      case 300:
+      case 301:
+      case 302:
+      case 310:
+      case 311:
+      case 312:
+      case 313:
+      case 314:
+      case 321:
+      case 520:
+      case 521:
+      case 522:
+      case 531:
+        return drizzleIcon;
+      case 500:
+      case 501:
+      case 502:
+      case 503:
+      case 504:
+        return isDay ? rainIconD : rainIconN;
+      case 200:
+      case 201:
+      case 202:
+      case 210:
+      case 211:
+      case 212:
+      case 221:
+      case 230:
+      case 231:
+      case 232:
         return thunderIcon;
-      case "Snow":
+      case 511:
+      case 600:
+      case 601:
+      case 602:
+      case 611:
+      case 612:
+      case 613:
+      case 615:
+      case 616:
+      case 620:
+      case 621:
+      case 622:
         return snowIcon;
       default:
-        return sunIcon;
+        return dayIcon;
     }
   }
 
@@ -41,7 +101,7 @@ export default function WeatherHeadline() {
     <>
       <div>
         <div className="max-md:flex items-center justify-between md:-mt-10">
-          <img src={weatherIcon(climate)} alt="cloud" />
+          <img src={weatherIcon(id, time)} alt="cloud" />
           <div className="max-md:flex items-center max-md:space-x-4">
             <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
               {Math.round(temperature)}°
